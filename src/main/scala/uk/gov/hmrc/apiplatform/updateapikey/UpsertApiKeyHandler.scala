fb1286eae170d0552e16889e3bfedc1bce8dd50b
Chris Rowe <chris@zeroalpha.technology>
2019-06-21 15:35:08 +0100

    API-3850: Add newly created API Key to appropriate Usage Plan

--- src/main/scala/uk/gov/hmrc/apiplatform/updateapikey/UpsertApiKeyHandler.scala
+++ src/main/scala/uk/gov/hmrc/apiplatform/updateapikey/UpsertApiKeyHandler.scala
@@ -1,8 +1,9 @@
 package uk.gov.hmrc.apiplatform.updateapikey
 
-import com.amazonaws.services.lambda.runtime.{Context, LambdaLogger}
 import com.amazonaws.services.lambda.runtime.events.SQSEvent
+import com.amazonaws.services.lambda.runtime.{Context, LambdaLogger}
 import software.amazon.awssdk.services.apigateway.ApiGatewayClient
+import software.amazon.awssdk.services.apigateway.model.{CreateApiKeyRequest, CreateUsagePlanKeyRequest}
 import software.amazon.awssdk.services.sqs.SqsClient
 import uk.gov.hmrc.api_platform_manage_api.AwsApiGatewayClient.awsApiGatewayClient
 import uk.gov.hmrc.api_platform_manage_api.AwsIdRetriever
@@ -12,16 +13,72 @@ class UpsertApiKeyHandler(override val apiGatewayClient: ApiGatewayClient,
                           sqsClient: SqsClient,
                           environment: Map[String, String]) extends SqsHandler with AwsIdRetriever {
 
+  val UsagePlansEnvironmentVariable: String = "usage_plans"
+
   def this() {
     this(awsApiGatewayClient, SqsClient.create(), sys.env)
   }
 
   override def handleInput(input: SQSEvent, context: Context): Unit = {
     implicit val logger: LambdaLogger = context.getLogger
-    if (input.getRecords.size != 1) {
-      throw new IllegalArgumentException(s"Invalid number of records: ${input.getRecords.size}")
+
+    if (input.getRecords.size != 1) throw new IllegalArgumentException(s"Invalid number of records: ${input.getRecords.size}")
+
+    val usagePlans: Map[String, String] = usagePlanIdsFromEnvironment()
+    val updateRequest: UpdateAPIKeyRequest = fromJson[UpdateAPIKeyRequest](input.getRecords.get(0).getBody)
+
+    val requestedUsagePlanId: String  = usagePlans.get(updateRequest.usagePlan) match {
+      case Some(usagePlanId) => usagePlanId
+      case None => throw new IllegalArgumentException(s"Requested Usage Plan [${updateRequest.usagePlan}] does not exist")
     }
 
+    val findAPIKeyIdResult: (String, Boolean) = findApiKeyId(updateRequest)
+    if (isNewAPIKey(findAPIKeyIdResult)) {
+      addAPIKeyToUsagePlan(findAPIKeyIdResult._1, requestedUsagePlanId)
+    } else {
+      moveAPIKeyToUsagePlan(findAPIKeyIdResult._1, requestedUsagePlanId)
+    }
+  }
+
+  def isNewAPIKey(findAPIKeyIdResult: (String, Boolean)): Boolean = findAPIKeyIdResult._2
+
+  def usagePlanIdsFromEnvironment(): Map[String, String] = {
+    environment.get(UsagePlansEnvironmentVariable) match {
+      case Some(usagePlansJson) => fromJson[Map[String, String]](usagePlansJson)
+      case None => throw new RuntimeException(s"No Usage Plans found under Environment Variable [$UsagePlansEnvironmentVariable]")
+    }
+  }
+
+  def findApiKeyId(updateRequest: UpdateAPIKeyRequest)(implicit logger: LambdaLogger): (String, Boolean) =
+    getAwsApiKeyIdByApplicationName(updateRequest.apiKeyName) match {
+      case Some(apiKeyId) =>
+        logger.log(s"Found existing API Key Id [$apiKeyId] for Application [${updateRequest.apiKeyName}]")
+        (apiKeyId, false)
+      case None =>
+        val newAPIKeyId = createNewApiKey(updateRequest.apiKeyName, updateRequest.apiKeyValue)
+        logger.log(s"Created API Key with Id [$newAPIKeyId] for Application [${updateRequest.apiKeyName}]")
+        (newAPIKeyId, true)
+    }
+
+  def createNewApiKey(apiKeyName: String, apiKeyValue: String): String =
+    apiGatewayClient.createApiKey(
+      CreateApiKeyRequest.builder()
+        .name(apiKeyName)
+        .value(apiKeyValue)
+        .build())
+      .id()
+
+  def addAPIKeyToUsagePlan(apiKeyId: String, usagePlanId: String) =
+    apiGatewayClient.createUsagePlanKey(
+      CreateUsagePlanKeyRequest.builder()
+        .keyId(apiKeyId)
+        .usagePlanId(usagePlanId)
+        .keyType("API_KEY")
+        .build())
+
+  def moveAPIKeyToUsagePlan(apiKeyId: String, usagePlanId: String): Unit = {
 
   }
 }
+
+case class UpdateAPIKeyRequest(usagePlan: String, apiKeyName: String, apiKeyValue: String)
--- src/test/scala/uk/gov/hmrc/apiplatform/updateapikey/UpsertApiKeyHandlerSpec.scala
+++ src/test/scala/uk/gov/hmrc/apiplatform/updateapikey/UpsertApiKeyHandlerSpec.scala
@@ -1,12 +1,18 @@
 package uk.gov.hmrc.apiplatform.updateapikey
 
+import java.util.UUID
+
 import com.amazonaws.services.lambda.runtime.events.SQSEvent
 import com.amazonaws.services.lambda.runtime.events.SQSEvent.SQSMessage
 import com.amazonaws.services.lambda.runtime.{Context, LambdaLogger}
+import org.mockito.ArgumentCaptor
+import org.mockito.ArgumentMatchers.any
 import org.mockito.Mockito.when
+import org.mockito.stubbing.OngoingStubbing
 import org.scalatest.mockito.MockitoSugar
 import org.scalatest.{Matchers, WordSpecLike}
 import software.amazon.awssdk.services.apigateway.ApiGatewayClient
+import software.amazon.awssdk.services.apigateway.model._
 import software.amazon.awssdk.services.sqs.SqsClient
 
 import scala.collection.JavaConversions._
@@ -14,10 +20,27 @@ import scala.collection.JavaConversions._
 class UpsertApiKeyHandlerSpec extends WordSpecLike with Matchers with MockitoSugar {
 
   trait Setup {
+    def getApiKeysCallReturns(existingAPIKeys: List[(String, String, String)]): OngoingStubbing[GetApiKeysResponse] = {
+      when(mockAPIGatewayClient.getApiKeys(any[GetApiKeysRequest]))
+        .thenReturn(
+          GetApiKeysResponse.builder()
+            .items(
+              existingAPIKeys.map((apiKey: (String, String, String)) => ApiKey.builder().id(apiKey._1).name(apiKey._2).value(apiKey._3).build()))
+            .build())
+    }
+
     val mockAPIGatewayClient: ApiGatewayClient = mock[ApiGatewayClient]
     val mockSqsClient: SqsClient = mock[SqsClient]
 
-    val environment: Map[String, String] = Map()
+    val usagePlans: Map[String, String] =
+      Map(
+        "BRONZE" -> UUID.randomUUID().toString,
+        "SILVER" -> UUID.randomUUID().toString,
+        "GOLD" -> UUID.randomUUID().toString,
+        "PLATINUM" -> UUID.randomUUID().toString)
+
+    val environment: Map[String, String] =
+      Map("usage_plans" -> usagePlans.map(plan => s""" "${plan._1}" : "${plan._2}" """).mkString("{", ",", "}"))
 
     val upsertApiKeyHandler = new UpsertApiKeyHandler(mockAPIGatewayClient, mockSqsClient, environment)
   }
@@ -33,7 +56,59 @@ class UpsertApiKeyHandlerSpec extends WordSpecLike with Matchers with MockitoSug
       sqsEvent
     }
 
-    def updateMessage(): SQSMessage = new SQSMessage()
+    def updateMessage(usagePlan: String, apiKeyName: String, apiKeyValue: String): SQSMessage = {
+      val messageBody = s"""{"usagePlan": "$usagePlan", "apiKeyName": "$apiKeyName", "apiKeyValue": "$apiKeyValue" }"""
+      val message = new SQSMessage()
+      message.setBody(messageBody)
+
+      message
+    }
+
+    def validSQSEvent(usagePlan: String, apiKeyName: String, apiKeyValue: String): SQSEvent =
+      buildSQSEvent(List(updateMessage(usagePlan, apiKeyName, apiKeyValue)))
+
+    "create API Key if it does not exist and adds it to Usage Plan" in new Setup {
+      val usagePlan: String = "BRONZE"
+      val apiKeyName: String = UUID.randomUUID().toString
+      val apiKeyValue: String = UUID.randomUUID().toString
+      val generatedApiKeyId: String = UUID.randomUUID().toString
+
+      getApiKeysCallReturns(List.empty)
+
+      val createApiKeyRequestCaptor: ArgumentCaptor[CreateApiKeyRequest] = ArgumentCaptor.forClass(classOf[CreateApiKeyRequest])
+      when(mockAPIGatewayClient.createApiKey(createApiKeyRequestCaptor.capture())).thenReturn(CreateApiKeyResponse.builder().id(generatedApiKeyId).build())
+
+      val createUsagePlanKeyRequestCaptor: ArgumentCaptor[CreateUsagePlanKeyRequest] = ArgumentCaptor.forClass(classOf[CreateUsagePlanKeyRequest])
+      when(mockAPIGatewayClient.createUsagePlanKey(createUsagePlanKeyRequestCaptor.capture())).thenReturn(CreateUsagePlanKeyResponse.builder().build())
+
+      upsertApiKeyHandler.handleInput(buildSQSEvent(Seq(updateMessage(usagePlan, apiKeyName, apiKeyValue))), mockContext)
+
+      val capturedCreateApiKeyRequest: CreateApiKeyRequest = createApiKeyRequestCaptor.getValue
+      capturedCreateApiKeyRequest.name() shouldBe apiKeyName
+      capturedCreateApiKeyRequest.value() shouldBe apiKeyValue
+
+      val capturedUsagePlanKeyRequest: CreateUsagePlanKeyRequest = createUsagePlanKeyRequestCaptor.getValue
+      capturedUsagePlanKeyRequest.keyId() shouldBe generatedApiKeyId
+      capturedUsagePlanKeyRequest.usagePlanId() shouldBe usagePlans(usagePlan)
+      capturedUsagePlanKeyRequest.keyType() shouldBe "API_KEY"
+    }
+
+    "does not update Usage Plan if API Key is already associated with it" in new Setup {
+      val usagePlan: String = "BRONZE"
+      val apiKeyId: String = UUID.randomUUID().toString
+      val apiKeyName: String = UUID.randomUUID().toString
+      val apiKeyValue: String = UUID.randomUUID().toString
+
+      getApiKeysCallReturns(List((apiKeyId, apiKeyName, apiKeyValue)))
+
+      upsertApiKeyHandler.handleInput(validSQSEvent(usagePlan, apiKeyName, apiKeyValue), mockContext)
+
+
+    }
+
+    "moves API Key from one Usage Plan to another if requested" in new Setup {
+
+    }
 
     "throw exception if the event has no messages" in new Setup {
       val sqsEvent: SQSEvent = buildSQSEvent(List())
@@ -43,10 +118,31 @@ class UpsertApiKeyHandlerSpec extends WordSpecLike with Matchers with MockitoSug
     }
 
     "throw exception if the event has multiple messages" in new Setup {
-      val sqsEvent: SQSEvent = buildSQSEvent(List(updateMessage(), updateMessage()))
+      val sqsEvent: SQSEvent =
+        buildSQSEvent(
+          List(
+            updateMessage("BRONZE", "abc", "123"),
+            updateMessage("SILVER", "def", "456")))
 
       val exception: IllegalArgumentException = intercept[IllegalArgumentException](upsertApiKeyHandler.handleInput(sqsEvent, mockContext))
       exception.getMessage shouldEqual "Invalid number of records: 2"
+    }
+
+    "throw exception if Usage Plans are not provided in an Environment Variable" in {
+      val upsertApiKeyHandler = new UpsertApiKeyHandler(mock[ApiGatewayClient], mock[SqsClient], Map())
+
+      val exception =
+        intercept[RuntimeException](upsertApiKeyHandler.handleInput(buildSQSEvent(List(updateMessage("BRONZE", "abc", "123"))), mockContext))
+
+      exception.getMessage shouldEqual "No Usage Plans found under Environment Variable [usage_plans]"
+    }
+
+    "throw exception if requested Usage Plan does not exist" in new Setup {
+      val invalidUsagePlan = "foobar"
+      val exception: IllegalArgumentException =
+        intercept[IllegalArgumentException](upsertApiKeyHandler.handleInput(validSQSEvent(invalidUsagePlan, UUID.randomUUID().toString, UUID.randomUUID().toString), mockContext))
+
+      exception.getMessage shouldEqual s"Requested Usage Plan [$invalidUsagePlan] does not exist"
     }
   }
 }
