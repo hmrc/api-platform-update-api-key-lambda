ada08cf313e00b31f6dca970934f95fbc9815537
Chris Rowe <chris@zeroalpha.technology>
2019-06-21 11:53:03 +0100

    API-3850: Tidy up of tests

--- src/test/scala/uk/gov/hmrc/apiplatform/updateapikey/UpsertApiKeyHandlerSpec.scala
+++ src/test/scala/uk/gov/hmrc/apiplatform/updateapikey/UpsertApiKeyHandlerSpec.scala
@@ -3,13 +3,10 @@ package uk.gov.hmrc.apiplatform.updateapikey
 import com.amazonaws.services.lambda.runtime.events.SQSEvent
 import com.amazonaws.services.lambda.runtime.events.SQSEvent.SQSMessage
 import com.amazonaws.services.lambda.runtime.{Context, LambdaLogger}
-import org.mockito.ArgumentMatchers.any
 import org.mockito.Mockito.when
 import org.scalatest.mockito.MockitoSugar
 import org.scalatest.{Matchers, WordSpecLike}
 import software.amazon.awssdk.services.apigateway.ApiGatewayClient
-import software.amazon.awssdk.services.apigateway.model.EndpointType.REGIONAL
-import software.amazon.awssdk.services.apigateway.model._
 import software.amazon.awssdk.services.sqs.SqsClient
 
 import scala.collection.JavaConversions._
@@ -20,20 +17,15 @@ class UpsertApiKeyHandlerSpec extends WordSpecLike with Matchers with MockitoSug
     val mockAPIGatewayClient: ApiGatewayClient = mock[ApiGatewayClient]
     val mockSqsClient: SqsClient = mock[SqsClient]
 
-
-    val mockContext: Context = mock[Context]
-    when(mockContext.getLogger).thenReturn(mock[LambdaLogger])
-    when(mockAPIGatewayClient.getRestApi(any[GetRestApiRequest]))
-      .thenReturn(GetRestApiResponse.builder().endpointConfiguration(EndpointConfiguration.builder().types(REGIONAL).build()).build())
-//    when(mockAPIGatewayClient.getRestApis(any[GetRestApisRequest])).thenReturn(buildMatchingRestApisResponse(apiId, apiName))
-    when(mockAPIGatewayClient.getUsagePlan(any[GetUsagePlanRequest])).thenReturn(GetUsagePlanResponse.builder().build())
-
     val environment: Map[String, String] = Map()
 
     val upsertApiKeyHandler = new UpsertApiKeyHandler(mockAPIGatewayClient, mockSqsClient, environment)
   }
 
   "Update API Key Handler" should {
+    val mockContext: Context = mock[Context]
+    when(mockContext.getLogger).thenReturn(mock[LambdaLogger])
+
     def buildSQSEvent(messages: Seq[SQSMessage]): SQSEvent = {
       val sqsEvent = new SQSEvent()
       sqsEvent.setRecords(messages)
